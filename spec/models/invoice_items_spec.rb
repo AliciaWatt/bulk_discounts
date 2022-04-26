a39c1fb00e30ecde89931bce64c336c68e235c5d
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-26 16:46:48 -0600

    test: add relationships and discouted_revenue test

--- spec/models/invoice_items_spec.rb
+++ spec/models/invoice_items_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe InvoiceItem, type: :model do
-
-  describe 'relationships' do
+  describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_many(:discounts).through(:item) }
   end
 
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of(:quantity) }
     it { should validate_presence_of(:unit_price) }
     it { should validate_presence_of(:status) }
@@ -16,11 +16,9 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_numericality_of(:quantity).is_greater_than(0) }
     it { should validate_numericality_of(:unit_price).is_greater_than(0) }
 
-    let!(:status) { %i[packaged pending shipped]}
+    let!(:status) { %i[packaged pending shipped] }
   end
 
-  describe 'instance methods' do
-   
+  describe "instance methods" do
   end
-
 end
