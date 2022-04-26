2d577dca8824856b259e15d4b3c6f3b5f3896834
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-25 18:13:16 -0600

    add numericality validations

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Discount, type: :model do
   describe "validations" do
     it { should validate_presence_of(:discount) }
     it { should validate_presence_of(:quantity) }
+    it { should validate_numericality_of(:discount) }
+    it { should validate_numericality_of(:quantity) }
   end
 end
