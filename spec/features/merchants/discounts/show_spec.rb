bd54d993b944b371249edcbad5c20497636499b5
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-26 13:16:37 -0600

    test: link to edit discount

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -17,8 +17,22 @@ RSpec.describe "the merchant bulk discounts show" do
     visit "/merchants/#{merchant1.id}/discounts/#{discount1.id}"
 
     within ".discount-attributes" do
-      expect(page).to have_content("Discount : #{discount1.discount} %")
+      expect(page).to have_content("Discount: #{discount1.discount} %")
       expect(page).to have_content("Quantity Threshold: #{discount1.quantity}")
     end
+  end
+
+  it "has a link to edit the discount that routes to the edit page" do
+    merchant1 = Merchant.create!(name: "Show Page Sparkle")
+    discount1 = merchant1.discounts.create!(discount: 20, quantity: 10)
+
+    visit "/merchants/#{merchant1.id}/discounts/#{discount1.id}"
+
+    within ".edit-discount" do
+      expect(page).to have_link("Edit Discount")
+      click_link "Edit Discount"
+    end
+
+    expect(current_path).to eq("/merchants/#{merchant1.id}/discounts/#{discount1.id}/edit")
   end
 end
