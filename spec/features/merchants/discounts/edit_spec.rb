52a4dc4628de3a22cbeffd6b2af7858f4bdd82ca
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-26 13:17:24 -0600

    test: edit discount form

---
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+RSpec.describe "merchant discounts edit" do
+  it "populates form with existing attributes" do
+    merchant1 = Merchant.create!(name: "Edit Page Sparkle")
+    discount1 = merchant1.discounts.create!(discount: 20, quantity: 10)
+    # discount2 = merchant1.discounts.create!(discount: 50, quantity: 15)
+
+    visit "/merchants/#{merchant1.id}/discounts/#{discount1.id}/edit"
+
+    expect(page).to have_field(:quantity, with: 10)
+    expect(page).to have_field(:discount, with: 20.0)
+
+    fill_in(:quantity, with: 15)
+    fill_in(:discount, with: 50)
+
+    click_button("Update Discount")
+
+    expect(current_path).to eq("/merchants/#{merchant1.id}/discounts/#{discount1.id}")
+
+    expect(page).to have_content("Discount: 50.0 %")
+    expect(page).to have_content("Quantity Threshold: 15")
+    expect(page).not_to have_content("Discount: 10")
+    expect(page).not_to have_content("Quantity: 5")
+
+    expect(Discount.find(discount1.id).quantity).to eq(15)
+    expect(Discount.find(discount1.id).discount).to eq(50)
+  end
+end
