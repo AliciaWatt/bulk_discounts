54f465adfa7baebac6e42050a25e3fbab856195c
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-27 01:42:31 -0600

    fix: numericallity range

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,6 +4,6 @@ class Discount < ApplicationRecord
   validates_presence_of :discount
   validates_presence_of :quantity
   validates_numericality_of :quantity, :discount
-  validates :discount, inclusion: 0.1...0.99
+  validates :discount, numericality: {greater_than_or_equal_to: 0.01, less_than_or_equal_to: 0.99}
   validates :quantity, numericality: {greater_than: 0}
 end
